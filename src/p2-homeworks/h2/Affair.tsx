21b60f2b022f684f9e7f22e243e1c14e47442d4a
paraslov <paraslovjm@gmail.com>
2021-04-02 14:43:59 +0300

    Homework 2. Todolist: filter and delete tasks; typesation; layout.

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
+import s from './Affairs.module.css'
+import {AffairType} from './HW2';
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType // need to fix any
+    deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+    const deleteCallback = () => {
+        props.deleteAffairCallback(props.affair._id)
+    }
+    let priorityColor: string = ''
+    if (props.affair.priority === 'low') {
+        priorityColor = s.green
+    } else if (props.affair.priority === 'middle') {
+        priorityColor = s.orange
+    } else {
+        priorityColor = s.red
+    }
 
     return (
-        <div>
-            // show some text
+        <div className={s.wrapper}>
+            <div className={s.item}>{props.affair.name}</div>
+            <div className={`${s.item} ${priorityColor}`}>
+                {props.affair.priority}
+            </div>
 
-            <button onClick={deleteCallback}>X</button>
+            <button className={s.btn} onClick={deleteCallback}>X</button>
         </div>
     )
 }
